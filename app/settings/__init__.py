9ee95a5fa2b7d08bd8694d0ebf663a95bfc5b582
ohrstrom <ohrstrom@gmail.com>
2017-06-27 11:43:02 +0200

    use tox for tests

--- app/settings/__init__.py
+++ app/settings/__init__.py
@@ -27,71 +27,71 @@ include(
     scope=locals()
 )
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': '%(asctime)s %(lineno)-4s [%(levelname)s] %(name)s: %(message)s'
-        },
-        'debug': {
-            'format': '[%(levelname)s] %(name)s: %(message)s'
-        },
-        'colored': {
-            '()': 'colorlog.ColoredFormatter',
-            'format': '%(log_color)s%(asctime)s %(lineno)-4s%(name)-24s %(levelname)-8s %(message)s',
-            'log_colors': {
-                'DEBUG': 'cyan',
-                'INFO': 'bold_green',
-                'WARNING': 'yellow',
-                'ERROR': 'red',
-                'CRITICAL': 'bold_red',
-            },
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'colored',
-        },
-    },
-    'loggers': {
-
-        'django.db.backends': {
-            'level': 'ERROR',
-            'handlers': ['console',],
-            'propagate': False
-        },
-        '': {
-            'handlers': ['console',],
-            'level': 'ERROR',
-            'propagate': False
-        },
-        'django.request': {
-            'handlers': ['console'],
-            'level': 'ERROR',
-            'propagate': False
-        },
-        'celery': {
-            'handlers': ['console'],
-            'level': 'WARNING',
-            'propagate': False
-        },
-        'dev': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False
-        },
-        'echoprint_server': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False
-        },
-        'fprint': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False
-        },
-    }
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'formatters': {
+#         'standard': {
+#             'format': '%(asctime)s %(lineno)-4s [%(levelname)s] %(name)s: %(message)s'
+#         },
+#         'debug': {
+#             'format': '[%(levelname)s] %(name)s: %(message)s'
+#         },
+#         'colored': {
+#             '()': 'colorlog.ColoredFormatter',
+#             'format': '%(log_color)s%(asctime)s %(lineno)-4s%(name)-24s %(levelname)-8s %(message)s',
+#             'log_colors': {
+#                 'DEBUG': 'cyan',
+#                 'INFO': 'bold_green',
+#                 'WARNING': 'yellow',
+#                 'ERROR': 'red',
+#                 'CRITICAL': 'bold_red',
+#             },
+#         },
+#     },
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'colored',
+#         },
+#     },
+#     'loggers': {
+#
+#         'django.db.backends': {
+#             'level': 'ERROR',
+#             'handlers': ['console',],
+#             'propagate': False
+#         },
+#         '': {
+#             'handlers': ['console',],
+#             'level': 'ERROR',
+#             'propagate': False
+#         },
+#         'django.request': {
+#             'handlers': ['console'],
+#             'level': 'ERROR',
+#             'propagate': False
+#         },
+#         'celery': {
+#             'handlers': ['console'],
+#             'level': 'WARNING',
+#             'propagate': False
+#         },
+#         'dev': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#             'propagate': False
+#         },
+#         'echoprint_server': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#             'propagate': False
+#         },
+#         'fprint': {
+#             'handlers': ['console'],
+#             'level': 'DEBUG',
+#             'propagate': False
+#         },
+#     }
+# }
